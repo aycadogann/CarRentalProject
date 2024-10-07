b188f221d5fe8563505d5f05c3823163ef7258e7
Ayca Dogan <dogan.aycaa@gmail.com>
2024-10-07 12:41:29 +0300

    Add project files.

---
+++ DataAccess/Concrete/EntityFramework/EfCarDal.cs
@@ -0,0 +1,63 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.EntityFramework
+{
+    public class EfCarDal : ICarDal
+    {
+        public void Add(Car entity)
+        {
+            using (CarRentalContext carRentalContext=new CarRentalContext())
+            {
+                var addedCar = carRentalContext.Entry(entity);
+                addedCar.State = EntityState.Added;
+                carRentalContext.SaveChanges();
+            }
+        }
+
+        public void Delete(Car entity)
+        {
+            using (CarRentalContext carRentalContext = new CarRentalContext())
+            {
+                var deletedCar = carRentalContext.Entry(entity);
+                deletedCar.State = EntityState.Deleted;
+                carRentalContext.SaveChanges();
+            }
+        }
+
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            using (CarRentalContext carRentalContext = new CarRentalContext())
+            {
+                return carRentalContext.Set<Car>().SingleOrDefault(filter);
+            }
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            using (CarRentalContext carRentalContext=new CarRentalContext())
+            {
+                return filter == null
+                    ? carRentalContext.Set<Car>().ToList()
+                    : carRentalContext.Set<Car>().Where(filter).ToList();
+            }
+        }
+
+        public void Update(Car entity)
+        {
+            using (CarRentalContext carRentalContext = new CarRentalContext())
+            {
+                var updatedCar = carRentalContext.Entry(entity);
+                updatedCar.State = EntityState.Modified;
+                carRentalContext.SaveChanges();
+            }
+        }
+    }
+}
---
+++ DataAccess/Concrete/InMemory/InMemoryCarDal.cs
@@ -0,0 +1,75 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryCarDal : ICarDal
+    {
+        List<Car> _cars;
+
+        public InMemoryCarDal()
+        {
+
+            _cars = new List<Car>
+            {
+                new Car{ Id=1, BrandId=1, ColorId=1, CarName="A", DailyPrice=1233, ModelYear=new DateTime(2020,4,20), Description="Manuel"},
+                new Car{ Id=2, BrandId=1, ColorId=1, CarName="B", DailyPrice=1900, ModelYear=new DateTime(2022,4,20), Description="Manuel"},
+                new Car{ Id=3, BrandId=2, ColorId=2, CarName="C", DailyPrice=1350, ModelYear=new DateTime(2024,4,20), Description="Otomatik"},
+                new Car{ Id=4, BrandId=3, ColorId=2, CarName="D", DailyPrice=2000, ModelYear=new DateTime(2018,4,20), Description="Manuel"},
+            };
+        }
+
+        public void Add(Car car)
+        {
+            _cars.Add(car);
+        }
+
+        public void Delete(Car car)
+        {
+            Car carToDelete = _cars.SingleOrDefault(c => c.Id == car.Id);
+            _cars.Remove(carToDelete);
+        }
+
+        public Car Get(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Car Get(Expression<Func<Car, bool>> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetAll()
+        {
+            return _cars;
+        }
+
+        public List<Car> GetAll(Expression<Func<Car, bool>> filter = null)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Car> GetById(int id)
+        {
+            return _cars.Where(c => c.Id == id).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            Car carToUpdate = _cars.SingleOrDefault(c => c.Id == car.Id);
+            carToUpdate.BrandId = car.BrandId;
+            carToUpdate.ColorId = car.ColorId;
+            carToUpdate.CarName = car.CarName;
+            carToUpdate.DailyPrice = car.DailyPrice;
+            carToUpdate.Description = car.Description;
+            carToUpdate.ModelYear = car.ModelYear;
+        }
+    }
+}
